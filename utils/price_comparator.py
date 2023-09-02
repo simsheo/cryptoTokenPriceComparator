4192f21990e7ceb705df8f981b6c5eafc3a74cb0
simsheo <91950874+simsheo@users.noreply.github.com>
2023-09-02 12:14:13 +0530

    Update price_comparator.py

--- utils/price_comparator.py
+++ utils/price_comparator.py
@@ -3,14 +3,14 @@ import logging
 # Function to find percentage variation between price from two sources 
 def price_variation(dia_price, external_source_price ):
     diff_percentage =  abs(external_source_price - dia_price) / dia_price * 100
-    logging.info(f"The percentage variation between two sources for price is: {diff_percentage}")
+    logging.info(f"price_comparator:price_variation-->source price :{dia_price}, external price:{external_source_price}, percentage variation: {diff_percentage}")
     return diff_percentage
 
 #Function to compare price difference with threshold and highlight any breach
 def price_threshold_breached(diff_percentage,threshold_percent):
     if diff_percentage > threshold_percent:
-        logging.info("The percentage variation is more then threshold")
+        logging.info(f"price_comparator:price_variation-->The percentage variation is more then threshold:: diff {diff_percentage} , threshold {threshold_percent}")
         return True
     else:
-        logging.info("The percentage variation is more then threshold")
+        logging.info(f"price_comparator:price_variation-->The percentage variation is less threshold: diff {diff_percentage} , threshold {threshold_percent}")
         return False
