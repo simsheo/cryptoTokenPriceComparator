cea516f05aafcf4fc02589c5c8353987d78e7341
simsheo <91950874+simsheo@users.noreply.github.com>
2023-09-02 10:49:27 +0530

    Update url_constants.py

--- config/url_constants.py
+++ config/url_constants.py
@@ -1,4 +1,4 @@
-# url_constants.py
+# url_constants.py - This similar to properties file to keep all urls at one point 
 
 DIA_BASE_URL = "https://api.diadata.org/v1/assetQuotation"
 DIA_POLYGON_URL=f"{DIA_BASE_URL}/Polygon/0xa3Fa99A148fA48D14Ed51d610c367C61876997F1"
@@ -12,8 +12,6 @@ COINGECO_POLYGON_URL = f"{COINGECKO_BASEL_URL}/coins/markets"
 COINGECKO_FANTOM_URL= f"{COINGECKO_BASEL_URL}/simple/token_price/fantom"
 
 GRAPHQL_URL ="https://api.diadata.org/graphql/query"
-
-
 SLACK_URL="https://slack.com/api/chat.postMessage"
-#"https://hooks.slack.com/services/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"
+
 
