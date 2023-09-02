bf03e57de98169f5e5fff995467f95b0fe83df4a
simsheo <91950874+simsheo@users.noreply.github.com>
2023-09-02 10:47:37 +0530

    Update dia_rest_api.py

--- api/dia_rest_api.py
+++ api/dia_rest_api.py
@@ -1,14 +1,15 @@
+import logging
 import requests
 
-# Function to fetch MiMatic price data from a given endpoint
+# Function to fetch price data from a given endpoint
 def fetch_dia_price(api_endpoint):
     response = requests.get(api_endpoint)
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        logging.info(f"dia_rest_api:fetch_dia_price-->{data}")
         return data
     else:
-        print("Error fetching price data:", response.status_code)
+        logging.info(f"dia_rest_api:fetch_dia_price-->Error fetching price data:{response.status_code}")
         return None
 
 
