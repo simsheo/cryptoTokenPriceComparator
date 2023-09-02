37505bd9ceafe20f5a00f96093e33e61c2a40f02
simsheo <91950874+simsheo@users.noreply.github.com>
2023-09-02 10:58:27 +0530

    Update coingecko_rest_api.py
    Updated logging

--- api/coingecko_rest_api.py
+++ api/coingecko_rest_api.py
@@ -1,7 +1,9 @@
 import requests
+import logging
 
 # Function to fetch price data from CoinGecko using api rest points
 def fetch_coingecko_price(url, chain, token_address):
+    logging.info(f"coingecko_rest_api:fetch_coingecko_price--> API -{url}, chain: {chain}")
     # Checking the chain and setting the URL and parameters accordingly
     if chain == 'polygon':        
         params = {            
@@ -16,13 +18,12 @@ def fetch_coingecko_price(url, chain, token_address):
     
     # Sending a GET request to the CoinGecko API
     response = requests.get(url, params=params)
-    
-    
+       
     # Checking the response status code
     if response.status_code == 200:
         data = response.json()
-        print(data);
+        logging.info(f"coingecko_rest_api:fetch_coingecko_price--> data - {data}");
         return data
     else:
-        print(f"Error fetching token price from CoinGecko API: {response.status_code}")
+        logging.info(f"coingecko_rest_api:fetch_coingecko_price-->Error fetching token price from CoinGecko API: {response.status_code}")
         return None
